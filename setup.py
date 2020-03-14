c1f88a120ddfa266b4c220f92e81f514eced88d9
Ashot Vardanian <av@av19mac.local>
2020-03-13 19:55:07 -0500

    Package description update

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ import setuptools
 from pathlib import Path
 
 setuptools.setup(
-    name='igramscraper',
-    version='0.4.0',
-    description=('scrapes medias, likes, followers, tags and all metadata'),
+    name='ig',
+    version='0.5.0',
+    description=('Instagram scraper with proper pagination, that can collect posts, likes, comments and a lot more.'),
     long_description=Path('README.md').read_text(),
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     license='MIT',
-    maintainer='realsirjoe, leungwaiban, ashvardanian',
-    author='realsirjoe, leungwaiban, ashvardanian',
-    url='https://github.com/ashvardanian/instagram-scraper',
+    maintainer='ashvardanian, realsirjoe, leungwaiban',
+    author='ashvardanian, realsirjoe, leungwaiban',
+    url='https://github.com/ashvardanian/PyScrapeIg',
     install_requires=[
         'requests==2.21.0',
         'python-slugify==3.0.2'
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Beta',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
